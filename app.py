98d37c0994974b3d2d717aaf308a57844bbfdb40
jonastrabi <jonassenateur1@gmail.com>
2025-06-12 10:09:24 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "<h1>Bienvenue sur Moayé Boutique - Paradis des enfants</h1><p>Nos produits mixtes arrivent bientôt !</p>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
