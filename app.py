f06c26bb585c00c3ec9a069974e4f295681856fc
jonastrabi <jonassenateur1@gmail.com>
2025-06-13 20:24:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,5 +7,5 @@ app = Flask(__name__)
 def home():
     return "<h1>Bienvenue sur Moayé Boutique - Paradis des enfants</h1><p>Nos produits mixtes arrivent bientôt !</p>"
 
-if __name__ == "__main__":
-    app.run(debug=True)
+#if __name__ == "__main__":
+    #app.run(debug=True)
